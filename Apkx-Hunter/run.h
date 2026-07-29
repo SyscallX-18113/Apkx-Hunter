6c82312cc7e0a1977793feaa16f008b16d5de0cc
SyscallX-18113 <deveshkachhawaha951@gmail.com>
2026-07-29 16:24:03 +0530

    Add copyright and license comments to run.h
    Added copyright and license information to run.h

--- Apkx-Hunter/run.h
+++ Apkx-Hunter/run.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 Devesh Kachhawaha (SyscallX-18113)
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * See the LICENSE file in the project root for license information.
+ */
+
 #ifndef RUN_H
 #define RUN_H
 #include <stdio.h>
